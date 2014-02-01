f80929bcdcca2fb792455730825e957ca7940312
akkufix <xda@mkes.de>
2014-02-01 20:55:09 +0100

    first build - cleaned

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Sat Feb 1 20:52:47 CET 2014"
+#define LINUX_COMPILE_BY "me"
+#define LINUX_COMPILE_HOST "amd1800"
+#define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.4.10-akkufix@xda-g465902b-dirty"
